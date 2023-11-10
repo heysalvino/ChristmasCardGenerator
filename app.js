c69e212f8b71f840749d767a28b163a028723bbb
Jessica Salvino <150205230+heysalvino@users.noreply.github.com>
2023-11-10 15:21:01 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,19 +2,32 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-var path = require('path');
+const path = require('path');
 
-app.use(express.static('views'));
+// Serve static files from the 'public' directory
+app.use(express.static('public'));
 
+// Set the views directory using path.join for cross-platform compatibility
+app.set('views', path.join(__dirname, 'views'));
 
-app.set('views', __dirname + '/views');
-
+// Define routes
 app.get('/', function(req, res) {
     res.render('index.html');
 });
 
-app.get('/', function(req, res) {
+app.get('/account', function(req, res) {
     res.render('account_page.html');
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+// Handle 404 - Not Found
+app.use(function(req, res, next) {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
+
+// Handle 500 - Internal Server Error
+app.use(function(err, req, res, next) {
+    console.error(err.stack);
+    res.status(500).sendFile(path.join(__dirname, 'views', '500.html'));
+});
+
+app.listen(port, () => console.log(`App listening on port ${port}`));
